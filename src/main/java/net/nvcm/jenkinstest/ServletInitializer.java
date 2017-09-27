9dce7996c44a10c22aa2cbc4759bbb52b30bb58e
A.Sivakon <A.Sivakon@novacom.by>
2017-09-27 11:25:19 +0300

    do war deployment

---
+++ src/main/java/net/nvcm/jenkinstest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.nvcm.jenkinstest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsTestApplication.class);
+    }
+
+}
